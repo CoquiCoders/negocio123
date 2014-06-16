bfef093bab6d1754001035928c6b166386bba51d
Christian <christian.etpr10@gmail.com>
2014-06-16 16:38:51 -0700

    Add Agency model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Agency(models.Model):
+  class Meta:
+    verbose_name_plural = 'Agencies'
+
+  def __unicode__(self):
+      return self.full_name
+
+  full_name = models.CharField(max_length=50)
+  short_name = models.CharField(max_length=12)
