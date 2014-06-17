1e90079e8f996035a06a318e7a89e90c36ab1830
Christian <christian.etpr10@gmail.com>
2014-06-17 12:59:12 -0700

    Moved the project to Flask. Way simpler

--- core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from core.models import Agency
-
-
-class AgencyAdmin(admin.ModelAdmin):
-  fields = ('full_name', 'short_name')
-
-admin.site.register(Agency, AgencyAdmin)
--- core/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('',
-    url(r'^$', 'core.views.home', name='home'),
-    url(r'^agencia/(?P<agency_name>[(\w|\d|%)-]+)/$', 'core.views.agency', name='agency')
-)
